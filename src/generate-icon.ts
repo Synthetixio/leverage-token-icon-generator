59d583413ec945699d1acc3ed145dadfdc9aa593
Chase Manning <chase@manning.dev>
2024-07-19 14:13:47 +0100

    :rocket: add asset to icon

--- src/generate-icon.ts
+++ src/generate-icon.ts
@@ -1,37 +1,52 @@
 import { LeveragedToken } from ".";
-
-import * as PImage from "pureimage";
 import * as fs from "fs";
 
+const ASSET_ICON_PERCENT = 0.5;
+
+const { createCanvas, loadImage, Image } = require("canvas");
+
+export async function getAssetIcon(asset: string): Promise<string> {
+  const URL = `https://synthetixio.github.io/synthetix-assets/markets/${asset}.svg`;
+  const res = await fetch(URL);
+  return await res.text();
+}
+
 export async function generateIcon(
-  leveragedToken: LeveragedToken,
+  lt: LeveragedToken,
   px: number
 ): Promise<void> {
-  const symbol = `${leveragedToken.asset}${leveragedToken.leverage}${
-    leveragedToken.long ? "L" : "S"
-  }`;
+  const symbol = `${lt.asset}${lt.leverage}${lt.long ? "L" : "S"}`;
   console.log(`Generating: ${symbol} (${px}px)`);
 
-  // make image
-  const img1 = PImage.make(px, px);
+  // Create canvas
+  const canvas = createCanvas(px, px);
+  const ctx = canvas.getContext("2d");
 
-  // get canvas context
-  const ctx = img1.getContext("2d");
+  // Draw circle
+  ctx.beginPath();
+  ctx.arc(px / 2, px / 2, px / 2, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.fillStyle = "pink";
+  ctx.fill();
 
-  // fill with red
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, px, px);
+  // Draw Asset on canvas
+  const img = new Image();
+  const asssetSize = px * ASSET_ICON_PERCENT;
+  const start = (px - asssetSize) / 2;
+  img.onload = () => ctx.drawImage(img, start, start, asssetSize, asssetSize);
+  img.src = `./asset-icons/${lt.asset}.png`;
 
-  //write to 'out.png'
-  fs.mkdirSync("icons", { recursive: true });
+  // Write to file
   try {
-    await PImage.encodePNGToStream(
-      img1,
-      fs.createWriteStream(`icons/${symbol}.png`)
-    );
-    console.log(`Generated: ${symbol}`);
+    fs.mkdirSync("icons", { recursive: true });
+    const out = fs.createWriteStream(`icons/${symbol}.png`);
+    const stream = canvas.createPNGStream();
+    stream.pipe(out);
   } catch (e) {
     console.log(`Error generating: ${symbol}`);
     console.log(e);
+    return;
   }
+
+  console.log(`Generated: ${symbol}`);
 }
