81ff9f3132e5d3f34cccbf5fbda1e358bcb6cf7c
Chase Manning <chase@manning.dev>
2024-07-19 15:00:13 +0100

    :rocket: add color to leveraged tokens

--- src/generate-icon.ts
+++ src/generate-icon.ts
@@ -1,8 +1,9 @@
 import { LeveragedToken } from ".";
 import * as fs from "fs";
 import { createCanvas, Image } from "canvas";
+import ASSET_DATA from "./asset-data";
 
-const ASSET_ICON_PERCENT = 0.66666;
+const ASSET_ICON_PERCENT = 0.57;
 
 export async function generateIcon(
   lt: LeveragedToken,
@@ -10,6 +11,12 @@ export async function generateIcon(
 ): Promise<void> {
   const symbol = `${lt.asset}${lt.leverage}${lt.long ? "L" : "S"}`;
   console.log(`Generating: ${symbol} (${px}px)`);
+
+  const assetData = ASSET_DATA[lt.asset];
+  if (!assetData) {
+    console.log(`No asset data for: ${lt.asset}`);
+    return;
+  }
 
   // Create canvas
   const canvas = createCanvas(px, px);
@@ -19,8 +26,26 @@ export async function generateIcon(
   ctx.beginPath();
   ctx.arc(px / 2, px / 2, px / 2, 0, 2 * Math.PI);
   ctx.closePath();
-  ctx.fillStyle = "pink";
+  ctx.fillStyle = "#1f1f1f";
   ctx.fill();
+  if (assetData.secondaryColor) {
+    const gradient = ctx.createLinearGradient(0, px, px, 0);
+    gradient.addColorStop(0, assetData.color);
+    gradient.addColorStop(1, assetData.secondaryColor);
+    ctx.fillStyle = gradient;
+    ctx.globalAlpha = 0.4;
+    ctx.fill();
+    ctx.globalAlpha = 1;
+    ctx.globalCompositeOperation = "saturation";
+    ctx.fillStyle = "hsl(0,28%,50%)";
+    ctx.fill();
+    ctx.globalCompositeOperation = "source-over";
+  } else {
+    ctx.fillStyle = assetData.color;
+    ctx.globalAlpha = 0.4;
+    ctx.fill();
+    ctx.globalAlpha = 1;
+  }
 
   // Draw Asset on canvas
   const img = new Image();
@@ -31,8 +56,8 @@ export async function generateIcon(
 
   // Write to file
   try {
-    fs.mkdirSync("icons", { recursive: true });
-    const out = fs.createWriteStream(`icons/${symbol}.png`);
+    fs.mkdirSync(`icons/${lt.asset}`, { recursive: true });
+    const out = fs.createWriteStream(`icons/${lt.asset}/${symbol}.png`);
     const stream = canvas.createPNGStream();
     stream.pipe(out);
   } catch (e) {
